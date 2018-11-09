7a2c4c7c586d11b6ab8cf3ab93c8fd275244c6f5
forthorn <yanpeng@huomaotv.com>
2018-11-09 19:14:22 +0800

    first commit

---
+++ app/src/main/java/com/forthorn/thorn/MainActivity.java
@@ -0,0 +1,20 @@
+package com.forthorn.thorn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.forthorn.thorn.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
